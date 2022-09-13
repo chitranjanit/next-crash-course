783b364c992ba00f542742c0e2541a17c983780c
Chitranjan <chitranjan@Chitranjans-MacBook-Pro.local>
2022-09-13 07:52:02 +0530

    first init

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  const x = 3;
+
+  return (
+    <div>
+      <h1 className={styles.title}>
+        <span>WebDev</span>News
+      </h1>
+      <p className={styles.description}>Keep up to date with the latest news</p>
+    </div>
+  );
+};
+
+export default Header;
